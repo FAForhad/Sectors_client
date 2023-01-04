0be41b790d878be92f1f029de568ea5e7c9d9658
FAForhad <faforhad450@gmail.com>
2023-01-04 15:38:56 +0600

    form

--- src/Components/Sectors/Sectors.js
+++ src/Components/Sectors/Sectors.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
 
 const Sectors = () => {
+
+    const { data: sectors = [] } = useQuery({
+        queryKey: ['sectors'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/sectors`)
+            const data = await res.json()
+            return data
+        }
+    })
+
+
+    const [checked, setChecked] = useState(false)
+
+    const handleChecked = () => {
+        setChecked(!checked)
+    }
+
+    const storeData = (event) => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const select = form.select.value;
+        const checkbox = form.checkbox.value;
+        console.log(name, select, checkbox)
+    }
+
+    console.log(sectors)
     return (
         <div>
             <section id='sectors'>
@@ -8,10 +36,10 @@ const Sectors = () => {
                     <div class="grid grid-cols-1 lg:grid-cols-2">
                         <div class="relative z-10 lg:py-16">
                             <div class="relative h-64 sm:h-80 lg:h-full">
-                                <form>
+                                <form onSubmit={storeData}>
                                     <div className="mb-1 sm:mb-2">
                                         <label
-                                            htmlFor="firstName"
+                                            htmlFor="Name"
                                             className="inline-block mb-1 font-medium"
                                         >
                                             Name
@@ -21,44 +49,47 @@ const Sectors = () => {
                                             required
                                             type="text"
                                             className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                                            id="firstName"
-                                            name="firstName"
+
+                                            name="name"
                                         />
                                     </div>
-                                    <div className="mb-1 sm:mb-2">
-                                        <label
-                                            htmlFor="email"
-                                            className="inline-block mb-1 font-medium"
-                                        >
-                                            E-mail
-                                        </label>
-                                        <input
-                                            placeholder="john.doe@example.org"
-                                            required
-                                            type="text"
-                                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                                            id="email"
-                                            name="email"
-                                        />
-                                    </div>
+
                                     <div className="mb-1 sm:mb-2">
                                         <label
                                             htmlFor="sector"
                                             className="inline-block mb-1 font-medium"
                                         >
-                                            Your Sector
+                                            Select Your Sector
                                         </label>
                                         <select
-                                            placeholder="john.doe@example.org"
                                             required
                                             type="text"
-                                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                                            id="email"
-                                            name="email"
+                                            className="flex-grow w-full lg:h-52 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                                            name="select"
+                                            size='10'
+
                                         >
-                                            <option value=""></option>
+                                            {
+                                                sectors.map(sec => <option key={sec._id} value={sec.Sectors}> {sec.Sectors}</option>)
+                                            }
+
                                         </select>
                                     </div>
+
+                                    <div class="form-group form-check mb-6">
+                                        <input
+                                            onChange={handleChecked}
+                                            type="checkbox"
+                                            class=" appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-rose-600 checked:border-rose-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                            id="exampleCheck1"
+                                            value={
+                                                checked ? 'Agree to terms' : 'Not Agreed'
+                                            }
+                                            name='checkbox'
+                                        />
+                                        <label class="form-check-label inline-block text-gray-800" for="exampleCheck1">Agree to terms</label>
+                                    </div>
+
                                     <div className="mt-4 mb-2 sm:mb-4">
                                         <button
                                             type="submit"
@@ -71,13 +102,13 @@ const Sectors = () => {
                             </div>
                         </div>
 
-                        <div class="relative flex items-center bg-gray-100">
+                        <div class="relative flex items-center bg-gray-100 lg:mt-2 mt-24">
                             <span
                                 class="hidden lg:absolute lg:inset-y-0 lg:-left-16 lg:block lg:w-16 lg:bg-gray-100"
                             ></span>
 
                             <div class="p-8 sm:p-16 lg:p-24">
-                                <h2 class="text-4xl font-bold sm:text-5xl mt-14 text-rose-700">
+                                <h2 class="text-4xl font-bold sm:text-5xl  text-rose-700">
                                     Sectors
                                 </h2>
                                 <h2 class="text-2xl font-bold sm:text-3xl">
